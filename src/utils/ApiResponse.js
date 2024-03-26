7534db763fd76b76042c568a252582c8629c7943
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-03-26 14:05:20 +0530

    utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiRseponse {
+    constructor(statusCode , data , message="Sucesss"){
+        this.statusCode =statusCode;
+        this.data = data ;
+        this.message = message;
+        this.success = statusCode < 400;
+
+    }
+}
+
+

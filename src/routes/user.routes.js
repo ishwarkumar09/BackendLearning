78154dd8f5d233216caf05aa2d0854f633e3558f
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-03-29 15:09:00 +0530

    add middleware and login logout controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser,registerUser,logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 
 const router =Router();
@@ -17,6 +18,10 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+ router.route("/login").post(loginUser)
+
+ //secured routes
+ router.route("/logout").post(verifyJwt,logoutUser)
 
 
 
